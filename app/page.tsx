b82ff291a3fa1652e84a7e0081c26ebbb257e487
KUNJ DAVE <Kunjdave2703@gmail.com>
2026-05-31 12:31:41 +0530

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+import About from '@/components/About'
+import Projects from '@/components/Projects'
+import Skills from '@/components/Skills'
+import Experience from '@/components/Experience'
+import Blog from '@/components/Blog'
+import Contact from '@/components/Contact'
+import Footer from '@/components/Footer'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Skills />
+        <Experience />
+        <Blog />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  )
+}
